225a2d7a3e324d4bf71e8ff8f2fdd502f5946700
CHEGEBB <chegephil24@gmail.com>
2024-06-24 16:42:12 +0300

    corrected eslint

--- 0x00-ES6_basic/10-main.js
+++ 0x00-ES6_basic/10-main.js
@@ -1,3 +1,3 @@
-import appendToEachArrayValue from './10-loops.js';
+import appendToEachArrayValue from './10-loops';
 
 console.log(appendToEachArrayValue(['appended', 'fixed', 'displayed'], 'correctly-'));
