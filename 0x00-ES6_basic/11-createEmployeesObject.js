7d48ca29a3235eff11c1eb48b41705d7bdb94a2e
CHEGEBB <chegephil24@gmail.com>
2024-06-24 16:48:04 +0300

    new

---
+++ 0x00-ES6_basic/11-createEmployeesObject.js
@@ -0,0 +1,8 @@
+export default function createEmployeesObject(departmentName, employees) {
+    const employeesObject = {
+      [departmentName]: [...employees]
+    };
+  
+    return employeesObject;
+  }
+  
