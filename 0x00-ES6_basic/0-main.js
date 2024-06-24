a49f4795bd57497718b21a88c3e6a2db32d48716
CHEGEBB <chegephil24>
2024-06-24 11:39:37 +0300

    new

--- 0x00-ES6_basic/0-main.js
+++ 0x00-ES6_basic/0-main.js
@@ -1,4 +1,3 @@
-import { taskFirst, taskNext } from './0-constants.js';
-// new
-console.log('Starting)
+import { taskFirst, taskNext } from './0-constants';
+
 console.log(`${taskFirst()} ${taskNext()}`);
