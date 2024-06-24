8b09ef504357e7c15ff6d119547843fffd92aa37
CHEGEBB <chegephil24>
2024-06-24 15:25:07 +0300

    arrow

---
+++ 0x00-ES6_basic/2-main.js
@@ -0,0 +1,5 @@
+import GetNeighborhoodsList from './2-arrow';
+
+const neighborhoodsList = new GetNeighborhoodsList();
+const res = neighborhoodsList.addNeighborhood('Noe Valley');
+console.log(res);
