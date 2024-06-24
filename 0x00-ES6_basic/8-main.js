aad88cbc05d94f9745ea1a73e6a7f6de17e473c5
CHEGEBB <chegephil24@gmail.com>
2024-06-24 16:23:36 +0300

    eslint

--- 0x00-ES6_basic/8-main.js
+++ 0x00-ES6_basic/8-main.js
@@ -1,3 +1,3 @@
-import getBudgetForCurrentYear from './8-getBudgetCurrentYear.js';
+import getBudgetForCurrentYear from './8-getBudgetCurrentYear';
 
 console.log(getBudgetForCurrentYear(2100, 5200, 1090));
