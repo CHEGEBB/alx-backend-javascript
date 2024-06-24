9ee8fbc0a17c9ede823dc7af80b4ebfbc4bdfaa9
CHEGEBB <chegephil24@gmail.com>
2024-06-24 16:31:51 +0300

    new

---
+++ 0x00-ES6_basic/9-getFullBudget.js
@@ -0,0 +1,18 @@
+import getBudgetObject from './7-getBudgetObject';
+
+export default function getFullBudgetObject(income, gdp, capita) {
+  const budget = getBudgetObject(income, gdp, capita);
+  const fullBudget = {
+    income: budget.income,
+    gdp: budget.gdp,
+    capita: budget.capita,
+    getIncomeInDollars(income) {
+      return `$${income}`;
+    },
+    getIncomeInEuros(income) {
+      return `${income} euros`;
+    },
+  };
+
+  return fullBudget;
+}
