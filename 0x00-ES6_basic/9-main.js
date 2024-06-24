552bc0ea85d2304bb1e4e6e80226ac2d49cd6a25
CHEGEBB <chegephil24@gmail.com>
2024-06-24 16:34:14 +0300

    added main

--- 0x00-ES6_basic/9-main.js
+++ 0x00-ES6_basic/9-main.js
@@ -1,6 +1,6 @@
 import getFullBudgetObject from './9-getFullBudget';
 
 const fullBudget = getFullBudgetObject(20, 50, 10);
-
+// new Object
 console.log(fullBudget.getIncomeInDollars(fullBudget.income));
 console.log(fullBudget.getIncomeInEuros(fullBudget.income));
