0430ac5334ec9ce7e4e18815b66fabc545da5ff4
CHEGEBB <chegephil24@gmail.com>
2024-06-26 20:01:45 +0300

    new

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -1,12 +1,9 @@
-export default function getFullResponseFromApi(success) {
+export default function getFullResponseFromAPI(success) {
   const boolean = success;
   const promise = new Promise((resolve, reject) => {
     if (boolean) {
-      resolve({
-        status: 200,
-        body: 'Success',
-
-      });
+      resolve({ status: 200, body: 'Success' });
+    } else {
       reject(new Error('The fake API is not working currently'));
     }
   });
