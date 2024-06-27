a0471d37a9c722c948efc95a802d48105532ff2f
CHEGEBB <chegephil24@gmail.com>
2024-06-27 15:17:13 +0300

    done

--- 0x02-ES6_classes/100-main.js
+++ 0x02-ES6_classes/100-main.js
@@ -1,6 +1,6 @@
 import EVCar from './100-evcar';
 
-const ec1 = new EVCar("Tesla", "Turbo", "Red", "250");
+const ec1 = new EVCar('Tesla', 'Turbo', 'Red', '250');
 console.log(ec1);
 
 const ec2 = ec1.cloneCar();
