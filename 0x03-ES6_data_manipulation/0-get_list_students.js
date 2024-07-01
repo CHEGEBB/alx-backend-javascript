c045d4f98a4a5b6811d311b3d283bcdfebbc373e
CHEGEBB <chegephil24@gmail.com>
2024-07-01 09:58:10 +0300

    added array 1

---
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -0,0 +1,19 @@
+export default function getListtudents() {
+  return [
+    {
+      id: 1,
+      firstName: 'Guillaume',
+      location: 'San Francisco',
+    },
+    {
+      id: 2,
+      firstName: 'James',
+      location: 'Colombia',
+    },
+    {
+      id: 3,
+      firstName: 'Serena',
+      location: 'San Francisco',
+    },
+  ];
+}
