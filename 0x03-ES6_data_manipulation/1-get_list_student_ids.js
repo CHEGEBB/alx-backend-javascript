f2da04783bc132580f509c4a4b53080d461fb6f9
@BrianChege <123733116+CHEGEBB@users.noreply.github.com>
2024-07-02 23:12:28 +0300

    Update 1-get_list_student_ids.js

--- 0x03-ES6_data_manipulation/1-get_list_student_ids.js
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -1,7 +1,9 @@
-export default function getListStudentsIds(array) {
-  if (!(Array.isArray(array))) {
-    return [];
+export default function getStudentIdsSum(students) {
+  if (students instanceof Array) {
+    return students.reduce(
+      (prevStudent, curStudent) => prevStudent.id || prevStudent + curStudent.id,
+      0,
+    );
   }
-  const ids = array.map((element) => element.id);
-  return ids;
+  return 0;
 }
