3cc0ddcd67ea0a2437fbb076e9105f7d945af536
CHEGEBB <chegephil24@gmail.com>
2024-07-02 17:25:35 +0300

    corrected main

---
+++ 0x03-ES6_data_manipulation/1-main.js
@@ -0,0 +1,5 @@
+import getListStudentIds from './1-get_list_student_ids';
+import getListStudents from './0-get_list_students';
+
+console.log(getListStudentIds('hello'));
+console.log(getListStudentIds(getListStudents()));
