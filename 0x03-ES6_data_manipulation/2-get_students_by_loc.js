0adcb068fc2a3c39a455caf964d83650de47ffdc
CHEGEBB <chegephil24@gmail.com>
2024-07-02 19:04:06 +0300

    module2

--- 0x03-ES6_data_manipulation/2-get_students_by_loc.js
+++ 0x03-ES6_data_manipulation/2-get_students_by_loc.js
@@ -1,3 +1,3 @@
-export default function getStudentsByLocation(students, city) {
-  return students.filter((student) => student.location === city);
+export default function getListStudentsByLocation(students, location) {
+  return students.filter((student) => student.location === location);
 }
