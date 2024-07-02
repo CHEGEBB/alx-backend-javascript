36ba2a42218724b85109399844af43a6e907b3e1
CHEGEBB <chegephil24@gmail.com>
2024-07-02 17:40:09 +0300

    refactor: Update getListStudents function to fix typo in location name

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -1,19 +1,7 @@
-export default function getListtudents() {
-  return [
-    {
-      id: 1,
-      firstName: 'Guillaume',
-      location: 'San Francisco',
-    },
-    {
-      id: 2,
-      firstName: 'James',
-      location: 'Colombia',
-    },
-    {
-      id: 5,
-      firstName: 'Serena',
-      location: 'San Francisco',
-    },
-  ];
+export default function getListStudents() {
+  return ([
+    { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
+    { id: 2, firstName: 'James', location: 'Columbia' },
+    { id: 5, firstName: 'Serena', location: 'San Francisco' },
+  ]);
 }
